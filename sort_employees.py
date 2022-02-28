a6237053fa5e86a6357c7dc5b4297c919d14200c
grivanov <subs@gr-ivanov.com>
2022-02-28 07:56:35 +0200

    ver2 - pass

--- sort_employees.py
+++ sort_employees.py
@@ -1,13 +1,16 @@
 def sort_employees(employees, sort_by):
     # Write your code here.
-    sort_index = int()
+    # sort_index = int()
 
-    if sort_by == "name":
-        sort_index = 0
-    elif sort_by == "age":
-        sort_index = 1
-    elif sort_by == "salary":
-        sort_index = 2
+    # if sort_by == "name":
+    #     sort_index = 0
+    # elif sort_by == "age":
+    #     sort_index = 1
+    # elif sort_by == "salary":
+    #     sort_index = 2
+
+    sort_options = ["name", "age", "salary"]
+    sort_index = sort_options.index(sort_by)
 
     def sort_func(a):
         return a[sort_index]
